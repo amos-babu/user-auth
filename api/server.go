c6c1082e875a21fb0d7c2a0fa22274d32640605a
amos-babu <amos.babu@yahoo.com>
2025-11-25 15:45:53 +0300

    x

--- api/server.go
+++ api/server.go
@@ -4,8 +4,10 @@ import (
 	"net/http"
 )
 
+type Server struct{}
+
 // HealthCheckHandler handles the health check endpoint.
-func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
